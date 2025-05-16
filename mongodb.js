0815b5503d1609a75d597b0b610f06e6555d10ec
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-05-16 20:39:29 +0700

    Query Collection

--- mongodb.js
+++ mongodb.js
@@ -7,3 +7,58 @@ db.createCollection("orders");
 db.getCollectionNames();
 
 db.customers.find();
+
+db.customers.insertOne({
+  _id: "parjo",
+  name: "Parjo Raharjo",
+});
+
+db.products.insertMany([
+  {
+    _id: 1,
+    name: "Indomie Ayam Bawang",
+    price: new NumberLong("2000"),
+  },
+  {
+    _id: 2,
+    name: "Mie Sedap Soto",
+    price: new NumberLong("2000"),
+  },
+]);
+
+db.orders.insertOne({
+  _id: new ObjectId(),
+  total: new NumberLong("8000"),
+  items: [
+    {
+      product_id: 1,
+      price: new NumberLong("2000"),
+      quantity: new NumberInt("2"),
+    },
+    {
+      product_id: 2,
+      price: new NumberLong("2000"),
+      quantity: new NumberInt("2"),
+    },
+  ],
+});
+
+// select * from customers where _id = "parjo"
+db.customers.find({
+  _id: "parjo",
+});
+
+// select * from customers where name = "Parjo Raharjo"
+db.customers.find({
+  name: "Parjo Raharjo",
+});
+
+// select * from products where price = 2000
+db.products.find({
+  price: 2000,
+});
+
+// select * from orders where items.product_id = 1
+db.orders.find({
+  "items.product_id": 1,
+});
