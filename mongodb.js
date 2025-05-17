36aa06b26b6585e2f44a4b9fa78a906ac7a9673e
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-05-17 14:21:41 +0700

    Comparison Query Operator

--- mongodb.js
+++ mongodb.js
@@ -62,3 +62,54 @@ db.products.find({
 db.orders.find({
   "items.product_id": 1,
 });
+
+// insert product documents
+db.products.insertMany([
+  {
+    _id: 3,
+    name: "Pop Mie Rasa Bakso",
+    price: new NumberLong("2500"),
+    category: "food",
+  },
+  {
+    _id: 4,
+    name: "Samsung Galaxy S9+",
+    price: new NumberLong("10000000"),
+    category: "handphone",
+  },
+  {
+    _id: 5,
+    name: "Acer Predator XXI",
+    price: new NumberLong("25000000"),
+    category: "laptop",
+  },
+]);
+
+// select * from customers where _id = "parjo"
+db.customers.find({
+  _id: {
+    $eq: "parjo",
+  },
+});
+
+// select * from products where price > 2000
+db.products.find({
+  price: {
+    $gt: 2000,
+  },
+});
+
+db.products.find({
+  $and: [
+    {
+      category: {
+        $in: ["laptop", "handphone"],
+      },
+    },
+    {
+      price: {
+        $gt: 10000000,
+      },
+    },
+  ],
+});
